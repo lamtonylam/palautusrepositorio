e9079d0ed06fef7a0e7861bb074e6318ccc82bc2
Tony Lam <tony.lam@iki.fi>
2024-10-28 15:16:35 +0200

    viikko1:teht15 - Riippuvuuksien injektointi osa 2: NHL-tilastot

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,10 +1,9 @@
 from player_reader import PlayerReader
 
 
-
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, playerreader):
+        reader = playerreader
 
         self._players = reader.get_players()
 
@@ -16,10 +15,7 @@ class StatisticsService:
         return None
 
     def team(self, team_name):
-        players_of_team = filter(
-            lambda player: player.team == team_name,
-            self._players
-        )
+        players_of_team = filter(lambda player: player.team == team_name, self._players)
 
         return list(players_of_team)
 
@@ -28,11 +24,7 @@ class StatisticsService:
         def sort_by_points(player):
             return player.points
 
-        sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
-        )
+        sorted_players = sorted(self._players, reverse=True, key=sort_by_points)
 
         result = []
         i = 0
