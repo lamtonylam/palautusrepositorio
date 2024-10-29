bc23d752ec5069d9373764529bbaeaadbeb3d7cb
Tony Lam <tony.lam@iki.fi>
2024-10-29 20:44:30 +0200

    viikko2:teht5 - git branch accidentally named teht5 to teht3

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -6,7 +6,7 @@ logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{summa(x,y)}")
+print(f"{erotus(x,y)}")
 
 logger("lopetetaan")
