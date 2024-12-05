96abcb1766ea10400ab8e713e48ac8194c4ac9a0
Tony Lam <tony.lam@iki.fi>
2024-12-05 13:27:25 +0200

    viikko6: teht 2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,19 +1,22 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
-    )
+    matcher = And(Not(HasAtLeast(2, "goals")), PlaysIn("NYR"))
+
+    # matcher = And(HasFewerThan(2, "goals"), PlaysIn("NYR"))
 
     for player in stats.matches(matcher):
         print(player)
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
 
 
 if __name__ == "__main__":
