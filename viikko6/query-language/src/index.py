7d41f86dc5dbc0ceb97cceff8794c20ead1d1470
Tony Lam <tony.lam@iki.fi>
2024-12-05 16:13:39 +0200

    viikko6: teht 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -3,13 +3,36 @@ from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 
+class QueryBuilder:
+    # eka otetaan matcheriin kaikki pelaajat
+    # joka kerta kun esim käytetään plays_in niin pelaajalistaa kierrätetään
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+        
+    def plays_in(self, team):
+        return QueryBuilder(PlaysIn(team))
+
+    def has_at_least(self, amount, type):
+        return QueryBuilder(And(self._matcher, HasAtLeast(amount, type)))
+
+    def has_fewer_than(self, amount, type):
+        return QueryBuilder(And(self._matcher, HasFewerThan(amount, type)))
+
+    def build(self):
+        return self._matcher
+
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"), Or(PlaysIn("NYR"), PlaysIn("FLA"), PlaysIn("BOS"))
+    query = QueryBuilder()
+    matcher = (
+        query.plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
     )
 
     for player in stats.matches(matcher):
